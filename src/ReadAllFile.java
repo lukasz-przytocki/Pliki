725c665645a63d5e81b1ceb55542e5343ea49966
lukasz.przytocki <lukasz.przytocki@gmail.com>
2019-06-12 18:26:52 +0200

    Obliczanie ilości znaków, białych llinii i wyrazów. ReadAllFille.

--- src/ReadAllFile.java
+++ src/ReadAllFile.java
@@ -5,17 +5,23 @@ import java.util.Scanner;
 
 public class ReadAllFile {
     public static void main(String[] args) throws FileNotFoundException {
-        File file = new File("file.txt");
+        File file = new File("Plik.txt");
         Scanner scanner = new Scanner(file);
-        String allText="";
+        String line=null;
+        int allChars = 0;
+        int whiteChars = 0;
+        int wordsCount = 0;
         while (scanner.hasNextLine()){
-            allText= allText+ " " +scanner.nextLine();
+            line = scanner.nextLine();
+            allChars+=line.length();
+            whiteChars+=line.replace(" ", "").length();
+            wordsCount+=line.split(" ").length;
+//            System.out.println(Arrays.toString(line.split(" ")));
         }
-        System.out.println(allText);
-          System.out.println("Ilość znaków: "+allText.length());
 
-        String[] wyrazy = allText.split(" ");
-        System.out.println(Arrays.toString(wyrazy));
+        System.out.println(allChars);
+        System.out.println(whiteChars);
+        System.out.println(wordsCount);
 
 
     }
